7d2ee99efb433242e03da8f010631bd3ce57699c
Guglielmo <guglielmo.celata@gmail.com>
2011-04-04 13:03:47 +0200

    admin interface spiced up, status messages and errors are more verbose; urls are reachable in one click

--- admin.py
+++ admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from sitescheck.models import Content, Recipient
 
+
+
 class ContentAdmin(admin.ModelAdmin):    
-  list_display = ('title', 'verified_at', 'verification_status')
+  list_display = ('_linked_title', 'verified_at', '_status_and_message')
   search_fields = ('title', 'notes' )
   list_filter = ('verification_status', )
   fieldsets = (
@@ -15,5 +17,23 @@ class ContentAdmin(admin.ModelAdmin):
     })
   )
   readonly_fields = ('hashed', 'verified_at', 'verification_status', 'verification_error')
+  
+  def _linked_title(self, obj):
+    return '%s <a href="%s" target="_blank"><img src="/images/extlink.gif" alt="vai" title="vai alla url: %s"</a>' % (obj.title, obj.url, obj.url)
+  _linked_title.allow_tags = True
+  _linked_title.short_description = 'Identificativo della URL (con link)'
+  
+  def _status_and_message(self, obj):
+    status = obj.verification_status
+    if obj.verification_status == 0:
+      msg = "IMMUTATO"
+    elif obj.verification_status == 1:
+      msg = "CAMBIATO"
+    else:
+      msg = obj.verification_error
+      
+    return ("%s - %s" % (status, msg))
+  _status_and_message.short_description = 'Status'
+  
 admin.site.register(Content, ContentAdmin)
 admin.site.register(Recipient)
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,3 +7,24 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^', include(admin.site.urls)),
 )
+
+# static media (not for production!)
+if (settings.ENVIRONMENT != 'production'):
+  urlpatterns += patterns('',
+    (r'^robots.txt$', 'django.views.static.serve', 
+      { 'path' : "/robots.txt", 
+        'document_root': settings.TEMPLATE_DIRS[0],
+        'show_indexes': False } ),
+    (r'^favicon.ico$', 'django.views.static.serve', 
+      { 'path' : "/favicon.ico", 
+        'document_root': settings.TEMPLATE_DIRS[0],
+        'show_indexes': False } ),
+    (r'^css/(?P<path>.*)$', 'django.views.static.serve',
+      { 'document_root': "%s/css" % settings.TEMPLATE_DIRS[0]}),
+    (r'^images/(?P<path>.*)$', 'django.views.static.serve',
+      { 'document_root': "%s/images" % settings.TEMPLATE_DIRS[0]}),
+    (r'^fonts/(?P<path>.*)$', 'django.views.static.serve',
+      { 'document_root': "%s/fonts" % settings.TEMPLATE_DIRS[0]}),
+    (r'^javascripts/(?P<path>.*)$', 'django.views.static.serve',
+      { 'document_root': "%s/javascripts" % settings.TEMPLATE_DIRS[0]}),  
+  )
